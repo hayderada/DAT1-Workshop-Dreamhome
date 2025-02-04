af5754b34eb570f73347fb3aded6035468bdb87a
hayderada <hayder.amer@ada.ac.uk>
2025-02-04 15:23:31 +0000

    appearnaces

--- BuildingTables/createDB.sql
+++ BuildingTables/createDB.sql
@@ -1,22 +1,46 @@
-.open ddlPractice.sqlite
+.open checkExercise.sqlite
 
---delete the table so it can be re-created every time
-DROP TABLE IF EXISTS EmployeeID;
 
--- create our first table
-CREATE TABLE EmployeeID (
-    EmployeeID INTEGER PRIMARY KEY NOT NULL,
-    SalesPerson VARCHAR(25),
-    SalesOffice VARCHAR(25),
-    OfficeNumber VARCHAR(40),
-    Customer1 VARCHAR(11),
-    Customer2 VARCHAR(11),
-    Customer3 VARCHAR(11)
+--DROP TABLE IS EXITS, need to think the order about dropping as some are dependent
+DROP TABLE IF EXISTS contactsExercise;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS voter;
+DROP TABLE IF EXISTS books;
+
+
+-- Table1
+CREATE TABLE contactsExercise (
+    contact_id INTEGER PRIMARY KEY NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    CONSTRAINT chk_phone CHECK (LENGTH(phone) >= 10)
 );
 
---insert data into the table
-INSERT INTO EmployeeID (EmployeeID, SalesPerson, SalesOffice, OfficeNumber, Customer1, Customer2, Customer3)
-VALUES  ('1003','Mary Smith', 'Chicago', '312-555-1212', 'Ford', 'GM', ''),
-        ('1004','John Hunt', 'New York', '312-555-1212', 'Dell', 'HP', ''),
-        ('1005','Martin Hap', 'Chicago', '312-555-1212', 'Boeing', '' ,'' )
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    list_price DECIMAL (10,2) NOT NULL,
+    discount DECIMAL (10, 2) NOT NULL,
+    CONSTRAINT chk_prices CHECK(list_price >= discount AND list_price >= 0 AND discount >= 0)
+);
 
+CREATE TABLE voter (
+    voter_id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    dob DATE,
+    CONSTRAINT chk_age CHECK(age >= 18),
+    CONSTRAINT chk_voterid CHECK (voter_id IN (101, 102, 103, 104, 105, 106))
+);
+-- Could also do  CONSTRAINT chk_voterid CHECK (voter_id BETWEEN 101 AND 106)
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    price INTEGER,
+    disc_Price INTEGER,
+    description VARCHAR(1000),
+    CONSTRAINT chk_disc_price CHECK(disc_price < price AND disc_price > 0)
+);
